8f7a60de186f62a33e91d562f8f078f4fb9395ee
이준희 <juni416@naver.com>
2025-01-01 15:30:12 +0900

    init project

---
+++ src/main/java/com/springsecurityjwtlearn/SpringSecurityJwtLearnApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurityjwtlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtLearnApplication.class, args);
+    }
+
+}
